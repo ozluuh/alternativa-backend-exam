00e39e399af2d6d4eeb94982b7c82da76262b8be
Luís Paulino <ozluuh@gmail.com>
2022-02-23 12:42:48 -0300

    initial: project setup

---
+++ src/Api/Controllers/HomeController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace Api.Controllers
+{
+    [ApiController]
+    [Route("/api/v1/home")]
+    public class HomeController : ControllerBase
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public object Get()
+        {
+            return new { hello = "world" };
+        }
+    }
+}
