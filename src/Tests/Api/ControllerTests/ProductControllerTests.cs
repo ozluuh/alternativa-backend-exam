a5df81eec02fb0343a0c05b4b43468b882f1d74b
Luís Paulino <ozluuh@gmail.com>
2022-02-27 17:12:39 -0300

    tests: add get product list

--- src/Tests/Api/ControllerTests/ProductControllerTests.cs
+++ src/Tests/Api/ControllerTests/ProductControllerTests.cs
@@ -1,5 +1,7 @@
+using System.Collections.Generic;
 using System.Threading.Tasks;
 using Api.Controllers;
+using Domain.Entities;
 using Domain.Repositories;
 using Microsoft.AspNetCore.Mvc;
 using Moq;
@@ -10,10 +12,11 @@ namespace Tests.Api.ControllerTests
     public class ProductControllerTests
     {
         [Fact]
-        [Trait("Api", "Controller")]
+        [Trait("Product", "Controller")]
         public async Task GetProductList_ReturnsOkObjectResult_WhenSuccessful()
         {
             var mockRepo = new Mock<IProductRepository>();
+            mockRepo.Setup(repo => repo.GetAllAsync()).ReturnsAsync(new List<Product>());
             var controller = new ProductController(mockRepo.Object);
 
             var response = await controller.GetProductList();
